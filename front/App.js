04247ec6fdfd2cb6f3f2dc1bf562fe856809295f
Oniel1721 <59784349+Oniel1721@users.noreply.github.com>
2020-11-20 11:29:32 -0400

    Update App.js

--- front/App.js
+++ front/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import Home from './pages/Home'
-import './styles/app.css';
+// import './styles/app.css';
 
 function App() {
   return (
