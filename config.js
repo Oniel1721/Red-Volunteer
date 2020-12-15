cd3dc2a84fbac32dc4de18fd5f81fc33c5f750ed
Oniel_Santos <Oniel@gmail.com>
2020-12-15 16:57:35 -0400

    add remote database

--- config.js
+++ config.js
@@ -1,8 +1,16 @@
 module.exports = {
+    /*
     database:{
         username:"root",
         password:"Angular2302",
         database:"blooddonation",
         host:"localhost"
+    }
+    */
+    database:{
+        username:"teNtvwIcVn",
+        password:"4tOkdF7dA6",
+        database:"teNtvwIcVn",
+        host:"remotemysql.com"
     }
 }
